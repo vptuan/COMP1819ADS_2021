ae88878d5a4081545fd11bf4ae0597b46bc817ba
vptuan <vptuan@users.noreply.github.com>
2021-02-02 14:11:59 +0000

    Update array_queue.py

--- Lab_03/array_queue.py
+++ Lab_03/array_queue.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from ..exceptions import Empty
+from queue import Empty
 
 class ArrayQueue:
   """FIFO queue implementation using a Python list as underlying storage."""
