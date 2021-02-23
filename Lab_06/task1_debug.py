22c09f0ae8d1735113fe70fdb0390fa2630dca68
vptuan <vptuan@users.noreply.github.com>
2021-02-23 15:45:58 +0000

    Add files via upload

--- Lab_06/task1_debug.py
+++ Lab_06/task1_debug.py
@@ -6,8 +6,13 @@ def insertLeft(root,newBranch):
     print(temp, len(temp))
     if len(temp) > 1: #  already a subtree temp at left of root, add a new left with temp being the left branch of newBranch
         root.insert(1,[newBranch,temp,[]])
-    else: # if t is null, len = 0, no left yet
+    else: # if t is null, len = 0, adding the newBranch to root
         root.insert(1,[newBranch, [], []])
+    return root
+
+def insertLeft2(root,newBranch):
+    temp = root.pop(1) # remove left branch of root and assign to temp
+    root.insert(1,[newBranch,temp,[]]) # add newBranch as new left with temp being the left of newBranch
     return root
 
 def insertRight(root,newBranch):
@@ -16,6 +21,11 @@ def insertRight(root,newBranch):
 		root.insert(2,[newBranch,[],t])
 	else:
 		root.insert(2,[newBranch,[],[]])
+	return root
+
+def insertRight2(root,newBranch):
+	t = root.pop(2)
+	root.insert(2,[newBranch,[],t])
 	return root
 
 def getRootVal(root):
@@ -32,12 +42,13 @@ def getRightChild(root):
 
 tree = BinaryTree('a')
 print(tree)
-insertLeft(tree,'b')
+insertLeft2(tree,'b')
 # print("insertLeft ", tree)
 # insertLeft(getLeftChild(tree),'d')
 print(tree)
-insertLeft(tree,'c')
+insertLeft2(tree,'c')
 print(tree)
+
 
 # insertRight(tree,'c')
 # insertLeft(getRightChild(tree),'e')
